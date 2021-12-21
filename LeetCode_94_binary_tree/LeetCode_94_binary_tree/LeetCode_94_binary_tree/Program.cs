8e6501e01295b340b32be1ff27c3eb3b2fd22e03
Abdo <abdomohamedatef@gmail.com>
2021-12-22 00:47:06 +0200

    2nd commid

--- LeetCode_94_binary_tree/LeetCode_94_binary_tree/LeetCode_94_binary_tree/Program.cs
+++ LeetCode_94_binary_tree/LeetCode_94_binary_tree/LeetCode_94_binary_tree/Program.cs
@@ -4,6 +4,19 @@ using System;
 
 class LeetCodeBinaryTree
 {
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(int val = 0, TreeNode left = null!, TreeNode right = null!)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
 
     public static void Main(string[] args)
     {
