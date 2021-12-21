dcb6e0180e4f51688872bbeb342b601ea3bb46ec
Abdo <abdomohamedatef@gmail.com>
2021-12-21 23:11:53 +0200

    First commit

---
+++ LeetCode_94_binary_tree/LeetCode_94_binary_tree/LeetCode_94_binary_tree/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+class LeetCodeBinaryTree
+{
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World");
+    }
+}
